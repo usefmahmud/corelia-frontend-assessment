18e04f4187e62ac73b98ef8f96b435670ea1834b
Yousef Mahmoud <usef.mahmoud@outlook.com>
2025-12-18 01:24:32 +0200

    feat(contacts): add contacts slice with CRUD operations for contact management

---
+++ src/store/contacts-slice.ts
@@ -0,0 +1,74 @@
+import type { Contact } from '@/types';
+import { createSlice } from '@reduxjs/toolkit';
+
+interface ContactsState {
+  contacts: Contact[];
+}
+
+const initialState: ContactsState = {
+  contacts: [],
+};
+
+const slice = createSlice({
+  name: 'contacts',
+  initialState,
+  reducers: {
+    addContact(state, action: { payload: { contact: Omit<Contact, 'id'> } }) {
+      const { contact } = action.payload;
+
+      const isUserExist = state.contacts.find(
+        (c) => c.userId === contact.userId
+      );
+      if (!isUserExist) {
+        throw new Error('User does not exist');
+      }
+
+      const isContactExist = state.contacts.find(
+        (c) =>
+          c.userId === contact.userId &&
+          c.name === contact.name &&
+          c.number === contact.number
+      );
+      if (isContactExist) {
+        throw new Error('Contact already exists');
+      }
+
+      const newContact: Contact = {
+        id: state.contacts.length + 1,
+        ...contact,
+      };
+
+      state.contacts.push(newContact);
+    },
+
+    deleteContact(state, action: { payload: { contactId: number } }) {
+      const { contactId } = action.payload;
+      state.contacts = state.contacts.filter((c) => c.id !== contactId);
+    },
+
+    updateContact(
+      state,
+      action: {
+        payload: {
+          contactId: number;
+          updatedContact: Partial<Omit<Contact, 'id' | 'userId'>>;
+        };
+      }
+    ) {
+      const { contactId, updatedContact } = action.payload;
+      const contactIndex = state.contacts.findIndex((c) => c.id === contactId);
+
+      if (contactIndex === -1) {
+        throw new Error('Contact not found');
+      }
+
+      state.contacts[contactIndex] = {
+        ...state.contacts[contactIndex],
+        ...updatedContact,
+      };
+    },
+  },
+});
+
+export const { addContact, updateContact, deleteContact } = slice.actions;
+export default slice.reducer;
